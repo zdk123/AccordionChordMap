0c64b380378e357d93abe4c168472215e2953e40
Zachary Kurtz <zdkurtz@gmail.com>
2025-03-10 16:13:39 -0400

    add deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  base: "/AccordionChordMap/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
